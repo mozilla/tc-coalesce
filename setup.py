9e13e145007a783ff2f35d5d0e2fae7e10be6c8c
Jake Watkins <jwatkins@mozilla.com>
2015-11-04 12:01:28 -0800

    Add setup.py - package registered as tc-coalesce

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+      name='tc-coalesce',
+      version='0.1.0',
+      description='A task coalescing service for Taskcluster',
+      author='Jake Watkins',
+      author_email='jwatkins@mozilla.com',
+      url='https://github.com/dividehex/tc-coalesce',
+      py_modules=['tc-coalesce'],
+      license='MPL2',
+      extras_require={
+          'test': [
+              'nose',
+              'mock',
+              'pep8',
+              'pyflakes',
+              'coverage',
+          ]
+      })
