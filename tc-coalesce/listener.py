378ed7eb4ba86f46c14124f812303e32360b22da
Jake Watkins <jwatkins@mozilla.com>
2015-12-21 11:47:28 -0800

    Better log format

--- tc-coalesce/listener.py
+++ tc-coalesce/listener.py
@@ -183,7 +183,9 @@ def setup_log():
     log.setLevel(logging.DEBUG)
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('[%(asctime)s] [%(process)d] ' \
+                                  '[%(levelname)s] %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S +0000')
     console_handler.setFormatter(formatter)
     log.addHandler(console_handler)
     return log
