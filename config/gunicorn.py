dda846aaafd6b010b1c42d9f591b2b7136c854e4
Jake Watkins <jwatkins@mozilla.com>
2016-04-27 11:20:11 -0700

    Fixes improper use of from_envvar and logging setup

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -1,6 +1,11 @@
+import os
+
 forwarded_allow_ips = '*'
 secure_scheme_headers = {
     'X-FORWARDED-PROTOCOL': 'ssl',
     'X-FORWARDED-PROTO': 'https',
     'X-FORWARDED-SSL': 'on'
     }
+
+if os.getenv('DEBUG'):
+    loglevel = 'debug'
